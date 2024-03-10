dce079e06192d62f47aa8ec1305b62d0b45207bd
Amine Amatoury <aminov.02@hotmail.com>
2024-03-10 20:00:02 +0700

    Created API Gateway

---
+++ API-Gateway/src/main/java/ru/tsu/hits/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,42 @@
+package ru.tsu.hits.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/bank-account/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://localhost:8081/"))
+                .route(p -> p
+                        .path("/customer/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://localhost:8082/"))
+                .route(p -> p
+                        .path("/employee/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://localhost:8083/"))
+                .route(p -> p
+                        .path("/loan/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://localhost:8084"))
+                .route(p -> p
+                        .path("/user/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://localhost:8085"))
+                .build();
+    }
+}
