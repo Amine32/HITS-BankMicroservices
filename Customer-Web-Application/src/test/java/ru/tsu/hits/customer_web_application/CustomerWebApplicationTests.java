5fcb43395d8f65da779a87e4efd367e60a804de0
Amine Amatoury <aminov.02@hotmail.com>
2024-03-10 18:17:54 +0700

    Initializing Project Created Bank-Account-Service Created Customer-Web-Application Created Employee-Web-Application Created Loan-Service Created User-Account-Service

---
+++ Customer-Web-Application/src/test/java/ru/tsu/hits/customer_web_application/CustomerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tsu.hits.customer_web_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Employee-Web-Application/src/test/java/ru/tsu/hits/employee_web_application/EmployeeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tsu.hits.employee_web_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
