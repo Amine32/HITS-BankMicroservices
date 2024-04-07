20cd7582b344cc69d788b4e4232be4a0c3178241
Amine Amatoury <aminov.02@hotmail.com>
2024-04-08 04:50:19 +0700

    Added Websockets for transactions

---
+++ Core-Service/src/main/java/ru/tsu/hits/core_service/controller/TransactionWebSocketController.java
@@ -0,0 +1,17 @@
+package ru.tsu.hits.core_service.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import ru.tsu.hits.core_service.model.Transaction;
+
+@Controller
+public class TransactionWebSocketController {
+
+    @MessageMapping("/transaction")
+    @SendTo("/topic/transactions")
+    public Transaction sendTransactionUpdate(Transaction transaction) {
+        return transaction;
+    }
+}
+
--- Core-Service/src/main/java/ru/tsu/hits/core_service/service/TransactionService.java
+++ Core-Service/src/main/java/ru/tsu/hits/core_service/service/TransactionService.java
@@ -1,6 +1,7 @@
 package ru.tsu.hits.core_service.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import ru.tsu.hits.core_service.model.Transaction;
 import ru.tsu.hits.core_service.model.TransactionType;
@@ -15,6 +16,7 @@ import java.util.List;
 public class TransactionService {
 
     private final TransactionRepository transactionRepository;
+    private final SimpMessagingTemplate messagingTemplate;
 
     public void recordTransaction(Long accountId, BigDecimal amount, TransactionType type) {
         Transaction transaction = new Transaction();
@@ -25,6 +27,9 @@ public class TransactionService {
         transaction.setTransactionDate(LocalDateTime.now());
 
         transactionRepository.save(transaction);
+
+        // After saving the transaction, send an update
+        messagingTemplate.convertAndSend("/topic/transactions", transaction);
     }
 
     public List<Transaction> getTransactionsByAccountId(Long accountId) {
