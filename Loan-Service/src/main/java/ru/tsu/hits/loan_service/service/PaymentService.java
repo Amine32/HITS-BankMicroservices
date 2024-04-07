70a13e67d90a9f0f9d024394c06d9344cb52dee3
Amine Amatoury <aminov.02@hotmail.com>
2024-04-08 01:02:35 +0700

    Cleaned up

--- Loan-Service/src/main/java/ru/tsu/hits/loan_service/service/PaymentService.java
+++ Loan-Service/src/main/java/ru/tsu/hits/loan_service/service/PaymentService.java
@@ -1,7 +1,6 @@
 package ru.tsu.hits.loan_service.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.tsu.hits.loan_service.model.Payment;
 import ru.tsu.hits.loan_service.repository.PaymentRepository;
