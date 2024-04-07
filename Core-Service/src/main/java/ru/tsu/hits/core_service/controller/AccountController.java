47cf9eb27ca8a8b6199057bd986c9e4b6d4500c4
Amine Amatoury <aminov.02@hotmail.com>
2024-04-08 01:53:04 +0700

    Cleaned Up

--- Core-Service/src/main/java/ru/tsu/hits/core_service/controller/AccountController.java
+++ Core-Service/src/main/java/ru/tsu/hits/core_service/controller/AccountController.java
@@ -4,7 +4,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import ru.tsu.hits.core_service.dto.AccountTransferDto;
 import ru.tsu.hits.core_service.model.Account;
