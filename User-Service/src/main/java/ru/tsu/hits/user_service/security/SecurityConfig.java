8f883d5314fccf10e6dabf2011274d98e43dfeef
Amine Amatoury <aminov.02@hotmail.com>
2024-04-11 03:19:10 +0700

    Fixed user creation

--- User-Service/src/main/java/ru/tsu/hits/user_service/security/SecurityConfig.java
+++ User-Service/src/main/java/ru/tsu/hits/user_service/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -34,6 +35,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
                         .requestMatchers("/api/users/authenticate").permitAll()
                         .anyRequest().authenticated()
                 )
