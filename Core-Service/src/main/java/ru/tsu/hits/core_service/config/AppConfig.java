fb009b17de68efcf5c7fee4dafa61aae749d42a2
Amine Amatoury <aminov.02@hotmail.com>
2024-04-22 09:13:11 +0700

    Added instability simulation

---
+++ Core-Service/src/main/java/ru/tsu/hits/core_service/config/AppConfig.java
@@ -0,0 +1,9 @@
+package ru.tsu.hits.core_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class AppConfig {
+}
