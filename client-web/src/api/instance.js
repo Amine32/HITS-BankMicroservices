65fc74af708c43db0a29b3c92a280788640b4d72
NeAnastasi <neanastasi@mail.ru>
2024-04-26 18:04:49 +0700

    fix

--- client-web/src/api/instance.js
+++ client-web/src/api/instance.js
@@ -3,13 +3,8 @@ import axios from "axios";
 export const instance = axios.create({
   headers: {
     "Authorization": "Bearer " + sessionStorage.getItem("authToken"),
-    "Idempotency-Key": generateIdempotencyKey(),
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
   },
 });
-
-function generateIdempotencyKey() {
-  return crypto.randomUUID();
-}
