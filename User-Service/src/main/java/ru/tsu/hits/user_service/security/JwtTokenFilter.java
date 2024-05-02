7136dbc8e9fe00dcde8794ed3ac28af0af006b90
Amine Amatoury <aminov.02@hotmail.com>
2024-05-02 21:48:13 +0700

    Added NonNull annotation

--- User-Service/src/main/java/ru/tsu/hits/user_service/security/JwtTokenFilter.java
+++ User-Service/src/main/java/ru/tsu/hits/user_service/security/JwtTokenFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -20,7 +21,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         String jwt = getJwtFromRequest(request);
