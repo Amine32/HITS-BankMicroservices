060153fe403b4ab870780e03662886735ea4f922
NeAnastasi <neanastasi@mail.ru>
2024-05-06 22:44:00 +0700

    fix

--- employee-web/src/components/Notification.js
+++ employee-web/src/components/Notification.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import toast, { Toaster } from 'react-hot-toast';
-import { requestForToken, onMessageListener } from './firebase';
+import { requestForToken, onMessageListener } from '../firebase';
 
 const Notification = () => {
   const [notification, setNotification] = useState({title: '', body: ''});
